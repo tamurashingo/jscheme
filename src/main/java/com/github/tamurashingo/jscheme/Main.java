7397a501ed4dabb84f3530299d0812ea176b2062
tamura shingo <tamura.shingo@gmail.com>
2018-10-14 23:36:47 +0900

    REPL追加

--- src/main/java/com/github/tamurashingo/jscheme/Main.java
+++ src/main/java/com/github/tamurashingo/jscheme/Main.java
@@ -1,4 +1,28 @@
 package com.github.tamurashingo.jscheme;
 
+import com.github.tamurashingo.jscheme.eval.Environment;
+import com.github.tamurashingo.jscheme.eval.Eval;
+import com.github.tamurashingo.jscheme.eval.Initialize;
+import com.github.tamurashingo.jscheme.parser.Parser;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class Main {
+
+    public static void main(String...args) throws Exception {
+        Eval eval = new Eval();
+        Environment env = Initialize.initEnvironment();
+
+        try (BufferedReader in = new BufferedReader(new InputStreamReader(System.in))) {
+            for (;;) {
+                System.out.print("> ");
+                String s = new String(in.readLine());
+                Parser parser = new Parser(s);
+                LObj exp = parser.parse();
+                LObj result = eval.eval(exp, env);
+                System.out.println(result.toString());
+            }
+        }
+    }
 }
